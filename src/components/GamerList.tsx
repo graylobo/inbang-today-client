d61196b3e7d3f61edf050495ff706fdc4f21013e
graylobo <cazador.min@gmail.com>
2025-02-09 18:28:31 +0900

    style

--- src/components/GamerList.tsx
+++ src/components/GamerList.tsx
@@ -124,7 +124,7 @@ function StarTier() {
       <div className="pt-20">
         {/* 기존 스트리머 그리드 */}
         <div
-          className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-4 p-4"
+          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8 gap-4 p-4"
           ref={streamerGridRef}
         >
           {filteredStreamers?.map((streamer) => {
