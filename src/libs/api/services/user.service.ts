07b249a3d54d512b43b55c230edb01bb41ec12de
graylobo <cazador.min@gmail.com>
2025-03-31 12:58:32 +0900

    chore: cookie

--- src/libs/api/services/user.service.ts
+++ src/libs/api/services/user.service.ts
@@ -7,7 +7,7 @@ export const getUserProfile = async () => {
   // 로컬 스토리지에서 토큰 가져오기
   const token = localStorage.getItem("access_token");
 
-  return api.get("/auth/me", {
+  return api.get(API_ROUTES.user.profile.get.url, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
