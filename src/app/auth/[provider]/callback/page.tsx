5a7aebe5fafc863418bf90e3811f4c574cf357a5
sio <cazador.min@gmail.com>
2025-04-04 14:05:46 +0900

    debug: log

--- src/app/auth/[provider]/callback/page.tsx
+++ src/app/auth/[provider]/callback/page.tsx
@@ -30,7 +30,6 @@ export default function AuthCallbackPage() {
       }
     };
 
-    console.log("useEffect:::");
     fetchUserProfile();
   }, [router, setUser, setTokens]);
 
--- src/libs/api/services/user.service.ts
+++ src/libs/api/services/user.service.ts
@@ -4,6 +4,8 @@ import { api } from "@/libs/api/axios";
 import { API_ROUTES } from "@/libs/api/route";
 
 export async function getUserProfile() {
+  console.log("getUserProfile start:::");
   const { data } = await api.get(API_ROUTES.user.profile.get.url);
+  console.log("getUserProfile end:::", data);
   return data;
 }
