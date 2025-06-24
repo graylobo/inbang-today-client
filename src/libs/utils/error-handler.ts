9acefb090c98be18b65a9e8dfc86138ff85f9920
graylobo <cazador.min@gmail.com>
2025-06-25 06:40:13 +0900

    refactor: error handling

--- src/libs/utils/error-handler.ts
+++ src/libs/utils/error-handler.ts
@@ -3,12 +3,9 @@
 import { ERROR_MESSAGES } from "@/common/constants/error-messages.enum";
 
 export const getErrorMessage = (error: any): string => {
-  if (!error) return ERROR_MESSAGES.DEFAULT;
 
-  let errorMessage = error.message || error.error;
-
-  if (errorMessage && ERROR_MESSAGES[errorMessage]) {
-    return ERROR_MESSAGES[errorMessage];
+  if (error && ERROR_MESSAGES[error]) {
+    return ERROR_MESSAGES[error];
   }
 
   return ERROR_MESSAGES.DEFAULT;
